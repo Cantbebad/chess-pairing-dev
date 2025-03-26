540d7eb00704b22f4c7c8e067a93d55d526fd4b2
brx <>
2025-03-26 13:23:58 +0100

    TODO - repair later

--- script.js
+++ script.js
@@ -861,6 +861,8 @@ class Controller {
 		if (!this.wasPairingGenerated()) return
 		let result = resultRow.result
 
+		// TODO: double rounded - does not change correctly, when result is changed
+
 		// two coresponding fields in the table are updated
 		let ind1 = resultRow.player1Idx
 		let ind2 = resultRow.player2Idx
